26aaa82d097a722fac0b62872613269a4a582d77
NMHien <nmhien2605@gmail.com>
2022-10-26 21:04:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [userData, setUserDate] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const registerUser = () => {
+    console.log(userData);
+    axios
+      .post("https://registration-backend.vercel.app/users/register", {
+        username: userData.username,
+        email: userData.email,
+        password: userData.password,
+        confirmPassword: userData.confirmPassword,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div>
+        <div>Username</div>
+        <input
+          value={userData.username}
+          onChange={(e) =>
+            setUserDate({ ...userData, username: e.target.value })
+          }
+        />
+      </div>
+      <div>
+        <div>Email</div>
+        <input
+          value={userData.email}
+          onChange={(e) => setUserDate({ ...userData, email: e.target.value })}
+        />
+      </div>
+      <div>
+        <div>Password</div>
+        <input
+          value={userData.password}
+          onChange={(e) =>
+            setUserDate({ ...userData, password: e.target.value })
+          }
+        />
+      </div>
+      <div>
+        <div>Confirm Password</div>
+        <input
+          value={userData.confirmPassword}
+          onChange={(e) =>
+            setUserDate({ ...userData, confirmPassword: e.target.value })
+          }
+        />
+      </div>
+      <button onClick={() => registerUser()}>Register</button>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +80,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
